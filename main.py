a6ee56fede2886f671f181e4a7065a1b4f3fedb6
glemiecha <g.lemiecha1@gmail.com>
2023-05-25 21:01:57 +0200

    define xls - xlsx conversion as function

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ from openpyxl import load_workbook, Workbook
 import pyexcel
 import csv
 
+xls_bom = "/home/grzegorz/Dev/bomtocsv/NABR009_OP20_BOM.xls"
+xlsx_bom = "/home/grzegorz/Dev/bomtocsv/NABR009_OP20_BOM.xlsx"
+
 #conversion to XLSX file
-pyexcel.save_book_as(file_name="/home/grzegorz/Dev/bomtocsv/NABR009_OP20_BOM.xls",
-                     dest_file_name="/home/grzegorz/Dev/bomtocsv/NABR009_OP20_BOM.xlsx")
+def convert_xls_to_xlsx(xls_path, xlsx_path):
+    pyexcel.save_book_as(file_name=xls_path,
+                        dest_file_name=xlsx_path)
 
 workbook = load_workbook("/home/grzegorz/Dev/bomtocsv/NABR009_OP20_BOM.xlsx")
 
