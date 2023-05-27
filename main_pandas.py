0a97de8342ce673e6ba907fb9c2ccc31ebadadf0
glemiecha <g.lemiecha1@gmail.com>
2023-05-27 15:29:50 +0200

    change main libraris from pyexcel and openpyxl to numpy and pandas. Add new main_pandas.py file

---
+++ main_pandas.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+# path to BOM.xls file
+xls_bom = "/home/grzegorz/Dev/bomtocsv/NABR009_OP20_BOM.xls"
+
+# convert .xls file to .csv
+read_file = pd.read_excel(xls_bom, sheet_name="Całość")
+read_file.to_csv(r"bom_temp.csv", index=None, header=True)
+
+# convert .csv to pandas DataFrame
+bom_csv = pd.read_csv("bom_temp.csv", skiprows=12)
+
+
+print(bom_csv.iloc[0])
