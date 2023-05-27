fdcaf773b732cb689ee157b29e22c82fd6cb51d8
glemiecha <g.lemiecha1@gmail.com>
2023-05-27 16:49:48 +0200

    add first version of converting bom file to prodio import dataframe

--- main_pandas.py
+++ main_pandas.py
@@ -1,16 +1,30 @@
 import pandas as pd
 
-PRODIO_IMPORT_HEADERS = ['Klient;\
-                         Oczekiwany termin realizacji;\
-                         Termin potwierdzony;\
-                         Zewn. nr zamówienia;\
-                         Produkt;\
-                         Sztuk;\
-                         Uwagi dla wszystkich;\
-                         Uwagi niewidoczne dla produkcji;\
-                         Atrybut 1 (opcjonalnie);\
-                         Atrybut 2 (opcjonalnie);\
-                         Atrybut 3 (opcjonalnie)']
+PRODIO_IMPORT_HEADERS = ['Klient',\
+                         'Oczekiwany termin realizacji',\
+                         'Termin potwierdzony',\
+                         'Zewn. nr zamówienia',\
+                         'Produkt',\
+                         'Sztuk',\
+                         'Uwagi dla wszystkich',\
+                         'Uwagi niewidoczne dla produkcji',\
+                         'Atrybut 1 (opcjonalnie)',\
+                         'Atrybut 2 (opcjonalnie)',\
+                         'Atrybut 3 (opcjonalnie)']
+
+PRODIO_EXAMPLE_ROW = ['NABR008',\
+                      '2023.05.30',
+                      '',
+                      '2023.05.27_GL_1',
+                      '22_018A_T_10_1_023',
+                      '3',
+                      'A3',
+                      'HOLDAX; PILNE',
+                      '',
+                      '',
+                      ''
+]
+
 
 # path to BOM.xls file
 xls_bom = "/home/grzegorz/Dev/bomtocsv/NABR009_OP20_BOM.xls"
@@ -23,4 +37,17 @@ read_file.to_csv(r"bom_temp.csv", index=None, header=True)
 bom_csv = pd.read_csv("bom_temp.csv", skiprows=12)
 
 prodio_df = pd.DataFrame(columns=PRODIO_IMPORT_HEADERS)
+
+for i in range(len(bom_csv)):
+    line = bom_csv.loc[i]
+    prodio_line = pd.Series(index=PRODIO_IMPORT_HEADERS)
+    prodio_line["Klient"] = "NATS012"
+    prodio_line["Oczekiwany termin realizacji"] = "2023.05.30"
+    prodio_line["Zewn. nr zamówienia"] = "2023.05.27_GL_1"
+    prodio_line["Produkt"] = line["Nazwa"]
+    prodio_line["Sztuk"] = line["Ilość"]
+    prodio_line["Uwagi dla wszystkich"] = "A1"
+    prodio_line["Uwagi niewidoczne dla produkcji"] = line["Gatunek"] + "; PILNE"
+    prodio_df.loc[len(prodio_df)] = prodio_line
+
 print(prodio_df)
