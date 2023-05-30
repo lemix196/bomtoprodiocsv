c733b6a2cba64539a3cd56c614e32867c2828f47
glemiecha <g.lemiecha1@gmail.com>
2023-05-30 21:07:50 +0200

    add templates dir with base.html and home.html

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def main():
-    return "<h2>This is my app!</h2>"
+def home():
+    return render_template('home.html')
