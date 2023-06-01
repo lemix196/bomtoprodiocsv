92d23b9c9e08749d0e630601928f0ec820bb1b71
glemiecha <g.lemiecha1@gmail.com>
2023-06-01 10:18:19 +0200

    change form method to post, show post in flask home view

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        print(request.form)
+        return render_template('home.html')
     return render_template('home.html')
