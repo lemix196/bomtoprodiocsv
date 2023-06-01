d142bffb5a755e3e933887339d8d547214be7e73
glemiecha <g.lemiecha1@gmail.com>
2023-06-01 17:25:38 +0200

    implement prodiocsv functionality in flask home view

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request
+import prodiocsv as pcsv
+import pandas as pd
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        filepath = request.form["file-path"]
+        finish_date = request.form["finish-date"]
+        prepare_date = request.form["prepare-date"]
+        is_urgent = request.form.getlist("isUrgent")
+        print(is_urgent)
+
+        ### CSV CREATION
+    # init of empty DataFrame object to be filled with lines to export to Prodio
+        machining_df = pd.DataFrame(columns=pcsv.PRODIO_IMPORT_HEADERS)
+        prepare_materials_df = pd.DataFrame(columns=pcsv.PRODIO_IMPORT_HEADERS)
+        client_name = pcsv.get_client_name(filepath)
+        bom_data = pcsv.convert_xls_to_dataframe(filepath)
+
+        # iteration through whole bom_csv file/DataFrame to pick data from it and swap to prodio csv
+        for i in range(len(bom_data)):
+            # read single line with i index
+            line = bom_data.loc[i]
+            p = pcsv.Product()
+            p.get_product_data(line)
+            pcsv.write_validated_line(product=p,
+                                validators=pcsv.validate_bom_line(p),
+                                prepare_dataframe=prepare_materials_df,
+                                machine_dataframe=machining_df,
+                                client_name=client_name,
+                                ext_order_number="nr_zamowienia",
+                                prepare_finish_date=prepare_date,
+                                finish_date=finish_date
+                                )
+
+        merged_df = pd.concat([machining_df, prepare_materials_df], ignore_index=True, sort=False)
+        merged_df.to_csv('generated_prodio_import.csv', sep=";", index=False)
+        ### END OF CSV CREATION
+        return render_template('home.html')
+    
+    return render_template('home.html')
