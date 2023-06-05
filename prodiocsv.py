0632f02f685d46809b788c3015bc8d8d876c2bbe
glemiecha <g.lemiecha1@gmail.com>
2023-06-05 20:20:08 +0200

    add functions automatically generating order number into prodiocsv module

--- prodiocsv.py
+++ prodiocsv.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import os
 from validators import ProductValidator
+import datetime as dt
+import re
 
 class Product:
     def __init__(self, product_name:str="",
@@ -34,7 +36,7 @@ PRODIO_IMPORT_HEADERS = ['Klient',\
                          'Atrybut 3 (opcjonalnie)']
 
 
-def convert_xls_to_dataframe(file) -> pd.read_excel():
+def convert_xls_to_dataframe(file) -> pd.read_excel:
     return pd.read_excel(file, sheet_name="Całość", skiprows=12, keep_default_na=False)
 
 
@@ -43,7 +45,60 @@ def get_client_name(path: str) -> str:
      return filename[1].split("_")[0]
 
 
-def validate_bom_line(product:Product) -> ProductValidator.validate_all():
+def today_date_to_str():
+     now = dt.date.today()
+     day = str(now.day) if now.day >= 10 else "0" + str(now.day)
+     month = str(now.month) if now.month >= 10 else "0" + str(now.month)
+     year = str(now.year)
+     return day + "."  + month + "." + year
+
+
+def is_str_date_later(date_to_check: str, reference_date: str) -> bool:
+     if int(date_to_check.split('.')[0]) > int(reference_date.split('.')[0]):
+          return True
+     else:
+          return False
+
+
+def create_order_id(initials: str) -> str:
+    date = today_date_to_str()
+    pattern = r"^[0-9]{2}\.[0-9]{2}\.[0-9]{4}\_" + str(initials)
+    ord_num = ""
+    # read log file with order
+    try:
+        # read all log file lines if file exists
+        with open('.ord_nums_log.txt', "r") as f:
+            lines = f.readlines()
+    except FileNotFoundError:
+        # create file if it does not exist
+        with open('.ord_nums_log.txt', "w") as f:
+            pass
+    # check whether log file needs clearing and then look for regex matches in existing lines
+    for line in lines:
+        # check if order numbers in log file are older than today
+        # if so - clear the log file and do not check lines pattern
+        if is_str_date_later(date, line.split("_")[0]):
+             with open('.ord_nums_log.txt', "w") as f:
+                pass
+             break
+        # if log file has order numbers only with today date - check which lines correspond to
+        # passed initials
+        if re.match(pattern, line):
+             ord_num = line[:-1]
+    
+    # create order number
+    if ord_num == "":
+         ord_num = date + "_" + initials + "_1"
+    else:
+         ord_num = ord_num[:-1] + str(int(ord_num[-1]) + 1 )
+
+    # write order number into log file
+    with open('.ord_nums_log.txt', "a") as f:
+        f.write(ord_num + "\n")
+    return ord_num
+
+
+def validate_bom_line(product:Product) -> ProductValidator.validate_all:
     # read data needed for product validation
     product_name = product.name
     material = str(product.material)
@@ -104,7 +159,3 @@ def write_validated_line(product:Product,
         prodio_line["Uwagi niewidoczne dla produkcji"] = product.material + "; PILNE" if is_urgent else product.material
         # insert pandas Series created above into dataframe
         machine_dataframe.loc[len(machine_dataframe)] = prodio_line
-
-
-if __name__ == "__main__":
-    pass
